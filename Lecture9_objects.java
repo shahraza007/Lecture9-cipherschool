1b6a7bf70277c99539485541c3f8e82126361cd0
shahraza007 <137696380+shahraza007@users.noreply.github.com>
2023-06-30 19:29:22 +0530

    Add files via upload

---
+++ Lecture9_objects.java
@@ -0,0 +1,14 @@
+class Students{
+    int rollno;
+    String name;
+}
+public class Lecture9_objects {
+    Students s1=new Students();
+    Students s2=new Students();
+    Students s3=new Students();
+    s1.name="Shahid";
+    s2.rollno=1;
+    s2.name="Ram";
+    s2.rollno=2
+
+}
